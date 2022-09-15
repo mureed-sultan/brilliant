f9ff4f635feb289f46c0a6135da73a5ef0676b0a
Mureed Sultan <mureedsultangeni@gmail.com>
2022-09-15 16:54:05 +0500

    New Way Design

--- src/scripts/app/site/pages/home.js
+++ src/scripts/app/site/pages/home.js
@@ -1,17 +1,39 @@
-import getComp from 'setjs/template/component.js';
-import todo from 'app/site/examples/todo.js';
+import getComp from 'setjs/template/component';
+
+const browseProduct = {
+  product1:{
+    img:'images/brilliant/brilliant06.jpeg',
+    desc:'Air Movement'
+  },
+    product2:{
+    img:'images/brilliant/brilliant07.jpg',
+    desc:'Commercial'
+  },    product3:{
+    img:'images/brilliant/brilliant08.jpg',
+    desc:'Electricial'
+  }, product4:{
+    img:'images/brilliant/brilliant09.jpg',
+    desc:'Indoor Lighning'
+  },product5:{
+    img:'images/brilliant/brilliant10.jpg',
+    desc:'Outdoor Lighting'
+  },product6:{
+    img:'images/brilliant/brilliant11.jpg',
+    desc:'Security'
+  },product7:{
+    img:'images/brilliant/brilliant02.jpg',
+    desc:'Smart Home'
+  },
+  product8:{
+    img:'images/brilliant/brilliant03.jpg',
+    desc:'Specifier Range'
+  },
+};
 
 export default {
-  templates: ['site/home'],
-  preload: function({success, error}) {
-    $.get('/templates/todo.html', function(todoTxt) {
-      success(todoTxt);
-   }, 'text').fail(error);
-  },
-  comp: function(opts, todoHtml) {
-    var pageComp = getComp('site/home');
-    pageComp.$todo.append(todo.comp().$root);
-    pageComp.$todoHtml[0].innerText = todoHtml;
-    return pageComp;
+  templates:['site/home'],
+  comp:()=>{
+    let page = getComp('site/home',{browseProduct});
+    return page;
   }
 };
